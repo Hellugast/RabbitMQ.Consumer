c8e3deacf507fc7ec8ddbd8534e5692c8f077572
Batu YURT <asdasd018@hotmail.com>
2023-06-19 21:57:31 +0300

    Basic consumer tested

--- RabbitMQ.Consumer/Program.cs
+++ RabbitMQ.Consumer/Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
 Console.WriteLine("Hello, World!");
+
+
+//Bağlantı oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://lhlfzvgo:***@crow.rmq.cloudamqp.com/lhlfzvgo");
+
+//Bağlantı aktifleştirme ve kanal açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Queue oluşturma
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+//Queue mesaj okuma
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "example-queue", false, consumer);
+consumer.Received += (sender, e) =>
+{
+    //Kuyruğa gelen mesajın işlendiği yerdir
+    //e.Body.ToArray() ya da e.Body.Span => Kuyruktaki mesajın byte verisi
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.ToArray()));
+};
+
+Console.ReadLine();
