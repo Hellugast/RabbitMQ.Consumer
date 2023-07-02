5d4c82ca17009e3e35a21044d65847db373a6a33
Batu YURT <asdasd018@hotmail.com>
2023-07-02 23:17:25 +0300

    MassTransit Request/Response Pattern

---
+++ RabbitMQ.ESB.MassTransit.RequestResponsePattern.Consumer/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MassTransit;
+using RabbitMQ.ESB.MassTransit.RequestResponsePattern.Consumer.Consumers;
+using RabbitMQ.ESB.MassTransit.Shared.RequestResponseMessages;
+
+Console.WriteLine("Hello, World!");
+
+string rabbitMQUri = "amqps://lhlfzvgo:***@crow.rmq.cloudamqp.com/lhlfzvgo";
+string requestQueue = "request-queue";
+
+var bus = Bus.Factory.CreateUsingRabbitMq(factory =>
+{
+    factory.Host(rabbitMQUri);
+
+    factory.ReceiveEndpoint(requestQueue, endpoint =>
+    {
+        endpoint.Consumer<RequestMessageConsumer>();
+    });
+});
+
+await bus.StartAsync();
+
+Console.ReadLine();
